878e1eab2492ffbc8b4eaccdcec113aaf6033040
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-02-10 10:39:32 -0300

    [fix] customers schema for cpf validation

--- src/schemas/customer.schemas.js
+++ src/schemas/customer.schemas.js
@@ -1,8 +1,10 @@
-import joi from 'joi';
+import joiBase from 'joi';
+import joiDate from '@joi/date';
+const joi = joiBase.extend(joiDate);
 
 export const customerSchema = joi.object({
-    name: joi.string().min(3).required(),
+    name: joi.string().min(2).required(),
     phone: joi.string().min(10).max(11).regex(/^\d+$/).required(),
-    cpf: joi.string().min(11).regex(/^\d+$/).required(),
-    birthday: joi.date().required()
+    cpf: joi.string().length(11).regex(/^\d+$/).required(),
+    birthday: joi.date().format('YYYY-MM-DD').utc().required()
 })
